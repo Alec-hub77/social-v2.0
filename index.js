031976dc6141a319c51b863047fd78086bd62fbf
Alec-hub77 <oleg.ostapiuk@yahoo.com>
2021-08-31 16:40:14 +0300

    upload img

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const morgan = require('morgan')
 const userRoute = require('./routes/users')
 const authRoute = require('./routes/auth')
 const postRoute = require('./routes/posts')
+const multer = require('multer')
+const path = require('path')
 
 dotenv.config();
 
@@ -17,10 +19,30 @@ mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopolo
     console.log("Connected to MongoDB")
 });
 
+app.use("/images", express.static(path.join(__dirname, "public/images")))
+
 // middleware
 app.use(express.json())
 app.use(helmet())
 app.use(morgan("common"))
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "public/images");
+    },
+    filename: (req, file, cb) => {
+        cb(null, req.body.name)
+    }
+})
+
+const upload = multer({storage: storage})
+app.post('/api/upload', upload.single("file"), (req, res) => {
+    try {
+        return res.status(200).json("File uploaded successfully.")
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
 
 app.use('/api/users', userRoute)
 app.use('/api/auth', authRoute)
--- routes/posts.js
+++ routes/posts.js
@@ -8,7 +8,7 @@ router.post('/', async (req, res) => {
   const newPost = new Post(req.body);
   try {
     const savedPost = await newPost.save();
-    res.status(201).json('Post has been created');
+    res.status(201).json(savedPost);
   } catch (error) {
     res.status(500).json('error');
   }
