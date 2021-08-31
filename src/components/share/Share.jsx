0896e304f5c2cabe398027284873cca198033c73
Alec-hub77 <oleg.ostapiuk@yahoo.com>
2021-08-31 16:40:48 +0300

    upload img

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,5 +1,6 @@
 import './share.scss'
 import {useContext, useRef, useState} from 'react'
+import axios from 'axios'
 import { AuthContext } from '../../contex/AuthContext';
 import {PermMedia, Label, Room, EmojiEmotions} from '@material-ui/icons';
 
@@ -8,6 +9,35 @@ function Share() {
     const PF = process.env.REACT_APP_PABLIC_FOLDER
     const [file, setFile] = useState(null)
     const desc = useRef();
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+
+        const newPost = {
+            userId: user._id,
+            desc: desc.current.value,
+        }
+        if(file) {
+            const data = new FormData();
+            const fileName = Date.now() + file.name;
+            data.append("name", fileName);
+            data.append("file", file);
+            newPost.img = fileName;
+            console.log(newPost);
+            try {
+                await axios.post("/upload", data)
+                window.location.reload();
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        try {
+           await axios.post('/posts', newPost)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <div className="share">
             <div className="shareWrapper">
@@ -16,7 +46,7 @@ function Share() {
                     <input ref={desc} placeholder={`Want to post some shit ${user.username} ?`} />
                 </div>
                 <hr className="shareHr"/>
-                <form className="shareBottom">
+                <form className="shareBottom" onSubmit={submitHandler}>
                     <div className="shareOptions">
                         <label htmlFor="file" className="shareOption">
                             <PermMedia htmlColor="red" className="shareIcon"/>
@@ -36,7 +66,7 @@ function Share() {
                             <span>Feelings</span>
                         </div>
                     </div>
-                    <button className="shareBtn">Share</button>
+                    <button className="shareBtn" type="submit">Share</button>
                 </form>
             </div>
         </div>
